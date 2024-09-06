c32117f27c18a4651b954c6a21be68df6c12a7b8
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-09-06 20:12:38 +0700

    Nhúng file tĩnh

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const routeClient = require("./routes/client/index.route");
 app.set('views', './views'); // Tìm đến thư mục tên là views
 app.set('view engine', 'pug'); // template engine sử dụng: pug
 
+app.use(express.static('public')); // Thiết lập thư mục chứa file tĩnh
+
 routeClient(app);
 
 app.listen(port, () => {
