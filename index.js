524948f3f5b0d19d7e696ef7724ab74110e3478a
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-09-06 19:50:25 +0700

    Cấu hình .env

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
+require('dotenv').config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 const routeClient = require("./routes/client/index.route");
 
