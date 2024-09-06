d8a1d047734a23a67eb7b6cf77dcbeda3f498eb8
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-09-06 19:36:48 +0700

    Cấu hình folder controllers

--- routes/client/home.route.js
+++ routes/client/home.route.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("client/pages/home/index");
-});
+const controller = require("../../controllers/client/home.controller");
+
+router.get("/", controller.index);
 
 module.exports = router;
--- routes/client/product.route.js
+++ routes/client/product.route.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("client/pages/products/index");
-});
+const controller = require("../../controllers/client/product.controller");
+
+router.get("/", controller.index);
 
 module.exports = router;
