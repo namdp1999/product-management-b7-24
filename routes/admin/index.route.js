8a283af4584e1e5f7fb962a1f2659af216ad6033
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-09-09 19:44:15 +0700

    1.3. Làm giao diện danh sách sản phẩm

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,5 +1,8 @@
 const dashboardRoute = require("./dashboard.route");
+const productRoute = require("./product.route");
 
 module.exports = (app) => {
   app.use("/admin/dashboard", dashboardRoute);
+  
+  app.use("/admin/products", productRoute);
 }
