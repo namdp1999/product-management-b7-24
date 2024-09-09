25be90ee94b1acc2f071ea083abb66f2f3d53fb8
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-09-09 20:13:24 +0700

    2. Lấy data sản phẩm in ra giao diện

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,5 +1,14 @@
-module.exports.index = (req, res) => {
+const Product = require("../../models/product.model");
+
+module.exports.index = async (req, res) => {
+  const products = await Product.find({
+    deleted: false
+  });
+
+  console.log(products);
+
   res.render("admin/pages/products/index", {
-    pageTitle: "Danh sách sản phẩm"
+    pageTitle: "Danh sách sản phẩm",
+    products: products
   });
 }
