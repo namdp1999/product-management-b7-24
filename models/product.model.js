02a50a950e31ee1c2e61eb4a5f9eb56672519f10
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-09-18 19:42:03 +0700

    4. Tính năng tạo mới sản phẩm - Thêm slug

--- models/product.model.js
+++ models/product.model.js
@@ -1,7 +1,14 @@
 const mongoose = require("mongoose");
+const slug = require('mongoose-slug-updater');
+mongoose.plugin(slug);
 
 const productSchema = new mongoose.Schema({
   title: String,
+  slug: {
+    type: String,
+    slug: "title",
+    unique: true
+  },
   description: String,
   price: Number,
   discountPercentage: Number,
