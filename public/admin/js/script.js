41a8adceaed01830dd996a6f497f14538a5914df
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-09-18 20:37:39 +0700

    4. Tính năng tạo mới sản phẩm - Preview ảnh

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -227,3 +227,18 @@ if(alertMessage) {
   }, 3000);
 }
 // End alert-message
+
+// Preview ảnh
+const uploadImage = document.querySelector("[upload-image]");
+if(uploadImage) {
+  const uploadImageInput = uploadImage.querySelector("[upload-image-input]");
+  const uploadImagePreview = uploadImage.querySelector("[upload-image-preview]");
+
+  uploadImageInput.addEventListener("change", () => {
+    const file = uploadImageInput.files[0];
+    if(file) {
+      uploadImagePreview.src = URL.createObjectURL(file);
+    }
+  });
+}
+// Hết Preview ảnh
