3b443c153f2851deca70f3e9385562ee3899c220
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-09-25 20:09:16 +0700

    3. Chỉnh sửa danh mục sản phẩm

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -36,3 +36,41 @@ module.exports.createPost = async (req, res) => {
 
   res.redirect(`/${systemConfig.prefixAdmin}/products-category`);
 }
+
+module.exports.edit = async (req, res) => {
+  const id = req.params.id;
+
+  const listCategory = await ProductCategory.find({
+    deleted: false
+  });
+
+  const category = await ProductCategory.findOne({
+    _id: id,
+    deleted: false
+  });
+
+  res.render("admin/pages/products-category/edit", {
+    pageTitle: "Chỉnh sửa danh mục sản phẩm",
+    listCategory: listCategory,
+    category: category
+  });
+}
+
+module.exports.editPatch = async (req, res) => {
+  const id = req.params.id;
+
+  if(req.body.position) {
+    req.body.position = parseInt(req.body.position);
+  } else {
+    delete req.body.position;
+  }
+
+  await ProductCategory.updateOne({
+    _id: id,
+    deleted: false
+  }, req.body);
+
+  req.flash("success", "Cập nhật thành công!");
+
+  res.redirect(`back`);
+}
--- routes/admin/product-category.route.js
+++ routes/admin/product-category.route.js
@@ -19,4 +19,13 @@ router.post(
   controller.createPost
 );
 
+router.get("/edit/:id", controller.edit);
+
+router.patch(
+  "/edit/:id",
+  upload.single('thumbnail'),
+  uploadCloud.uploadSingle,
+  controller.editPatch
+);
+
 module.exports = router;
