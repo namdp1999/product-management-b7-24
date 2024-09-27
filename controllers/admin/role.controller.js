c5f827275816cf0f4d65f857817e1125c36dcadb
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-09-27 19:28:21 +0700

    3.4. Xây dựng phần Chỉnh sửa nhóm quyền

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -24,3 +24,30 @@ module.exports.createPost = async (req, res) => {
 
   res.redirect(`/${systemConfig.prefixAdmin}/roles`);
 }
+
+module.exports.edit = async (req, res) => {
+  const id = req.params.id;
+
+  const role = await Role.findOne({
+    _id: id,
+    deleted: false
+  });
+
+  res.render("admin/pages/roles/edit", {
+    pageTitle: "Chỉnh sửa nhóm quyền",
+    role: role
+  });
+}
+
+module.exports.editPatch = async (req, res) => {
+  const id = req.params.id;
+
+  await Role.updateOne({
+    _id: id,
+    deleted: false
+  }, req.body);
+
+  req.flash("success", "Cập nhật thành công!");
+
+  res.redirect(`back`);
+}
