0a0b2b81a849573e12acffda27c87251290f7c6b
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-09-27 19:21:25 +0700

    3.3. Xây dựng phần Thêm mới nhóm quyền

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -1,4 +1,5 @@
 const Role = require("../../models/role.model");
+const systemConfig = require("../../config/system");
 
 module.exports.index = async (req, res) => {
   const records = await Role.find({
@@ -9,4 +10,17 @@ module.exports.index = async (req, res) => {
     pageTitle: "Nhóm quyền",
     records: records
   });
+}
+
+module.exports.create = async (req, res) => {
+  res.render("admin/pages/roles/create", {
+    pageTitle: "Tạo nhóm quyền"
+  });
+}
+
+module.exports.createPost = async (req, res) => {
+  const role = new Role(req.body);
+  await role.save();
+
+  res.redirect(`/${systemConfig.prefixAdmin}/roles`);
 }
