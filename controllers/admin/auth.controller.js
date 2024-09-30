493d4124d6eef7d850b0b12dc71afe17a1f02663
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-09-30 20:31:54 +0700

    2. Làm tính năng đăng nhập

---
+++ controllers/admin/auth.controller.js
@@ -0,0 +1,40 @@
+const Account = require("../../models/account.model");
+const md5 = require("md5");
+
+const systemConfig = require("../../config/system");
+
+module.exports.login = (req, res) => {
+  res.render("admin/pages/auth/login", {
+    pageTitle: "Đăng nhập"
+  });
+}
+
+module.exports.loginPost = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await Account.findOne({
+    email: email,
+    deleted: false
+  });
+
+  if(!user) {
+    req.flash("error", "Email không tồn tại!");
+    res.redirect("back");
+    return;
+  }
+
+  if(md5(password) != user.password) {
+    req.flash("error", "Sai mật khẩu!");
+    res.redirect("back");
+    return;
+  }
+
+  if(user.status != "active") {
+    req.flash("error", "Tài khoản đang bị khóa!");
+    res.redirect("back");
+    return;
+  }
+
+  res.cookie("token", user.token);
+  res.redirect(`/${systemConfig.prefixAdmin}/dashboard`);
+}
--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -3,6 +3,7 @@ const productRoute = require("./product.route");
 const productCategoryRoute = require("./product-category.route");
 const roleRoute = require("./role.route");
 const accountRoute = require("./account.route");
+const authRoute = require("./auth.route");
 
 const systemConfig = require("../../config/system");
 
@@ -18,4 +19,6 @@ module.exports = (app) => {
   app.use(`${PATH_ADMIN}/roles`, roleRoute);
 
   app.use(`${PATH_ADMIN}/accounts`, accountRoute);
+
+  app.use(`${PATH_ADMIN}/auth`, authRoute);
 }
