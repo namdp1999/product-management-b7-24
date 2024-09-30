b2939150acce9767a42eb4aad561c211649f2185
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-09-30 19:33:26 +0700

    1.3. Làm trang tạo tài khoản

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -1,5 +1,33 @@
+const Role = require("../../models/role.model");
+const Account = require("../../models/account.model");
+const md5 = require("md5");
+
+const generateHelper = require("../../helpers/generate.helper");
+const systemConfig = require("../../config/system");
+
 module.exports.index = (req, res) => {
   res.render("admin/pages/accounts/index", {
     pageTitle: "Tài khoản quản trị"
   });
+}
+
+module.exports.create = async (req, res) => {
+  const roles = await Role.find({
+    deleted: false
+  });
+
+  res.render("admin/pages/accounts/create", {
+    pageTitle: "Tạo tài khoản quản trị",
+    roles: roles
+  });
+}
+
+module.exports.createPost = async (req, res) => {
+  req.body.password = md5(req.body.password);
+  req.body.token = generateHelper.generateRandomString(30);
+  
+  const account = new Account(req.body);
+  await account.save();
+
+  res.redirect(`/${systemConfig.prefixAdmin}/accounts`);
 }
--- routes/admin/account.route.js
+++ routes/admin/account.route.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const multer  = require('multer');
+
+const upload = multer();
 
 const controller = require("../../controllers/admin/account.controller");
 
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
+
 router.get("/", controller.index);
+
+router.get("/create", controller.create);
+
+router.post(
+  "/create",
+  upload.single('avatar'),
+  uploadCloud.uploadSingle,
+  controller.createPost
+);
 
 module.exports = router;
