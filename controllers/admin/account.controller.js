db041ee107d09761765c4844104d47ee5d0bd99a
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-09-30 19:50:11 +0700

    1.5. Làm trang sửa tài khoản

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -45,3 +45,31 @@ module.exports.createPost = async (req, res) => {
 
   res.redirect(`/${systemConfig.prefixAdmin}/accounts`);
 }
+
+module.exports.edit = async (req, res) => {
+  const roles = await Role.find({
+    deleted: false
+  });
+
+  const account = await Account.findOne({
+    _id: req.params.id,
+    deleted: false
+  });
+
+  res.render("admin/pages/accounts/edit", {
+    pageTitle: "Chỉnh sửa tài khoản quản trị",
+    roles: roles,
+    account: account
+  });
+}
+
+module.exports.editPatch = async (req, res) => {
+  await Account.updateOne({
+    _id: req.params.id,
+    deleted: false
+  }, req.body);
+
+  req.flash("success", "Cập nhật thành công!");
+
+  res.redirect(`back`);
+}
--- routes/admin/account.route.js
+++ routes/admin/account.route.js
@@ -19,4 +19,13 @@ router.post(
   controller.createPost
 );
 
+router.get("/edit/:id", controller.edit);
+
+router.patch(
+  "/edit/:id",
+  upload.single('avatar'),
+  uploadCloud.uploadSingle,
+  controller.editPatch
+);
+
 module.exports = router;
