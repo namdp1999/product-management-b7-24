82b68cee4db1cc6d16d2fa5b046b2d7c106f20fd
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-10-04 20:25:03 +0700

    4. Làm trang chủ bên client

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,5 +1,81 @@
-module.exports.index = (req, res) => {
+const Product = require("../../models/product.model");
+
+module.exports.index = async (req, res) => {
+  // Sản phẩm nổi bật
+  const productsFeatured = await Product
+    .find({
+      deleted: false,
+      status: "active",
+      featured: "1"
+    })
+    .sort({
+      position: "desc"
+    })
+    .limit(6);
+
+  for (const item of productsFeatured) {
+    item.priceNew = (1 - item.discountPercentage/100) * item.price;
+    item.priceNew = item.priceNew.toFixed(0);
+  }
+
+  // Sản phẩm mới
+  const productsNew = await Product
+    .find({
+      deleted: false,
+      status: "active"
+    })
+    .sort({
+      position: "desc"
+    })
+    .limit(6);
+
+  for (const item of productsNew) {
+    item.priceNew = (1 - item.discountPercentage/100) * item.price;
+    item.priceNew = item.priceNew.toFixed(0);
+  }
+
+  // Sản phẩm giảm giá nhiều
+  const productsDiscount = await Product
+    .find({
+      deleted: false,
+      status: "active"
+    })
+    .sort({
+      discountPercentage: "desc"
+    })
+    .limit(6);
+
+  for (const item of productsDiscount) {
+    item.priceNew = (1 - item.discountPercentage/100) * item.price;
+    item.priceNew = item.priceNew.toFixed(0);
+  }
+
+  // Lấy ra các sản phẩm cụ thể (theo id)
+  const productsChoose = await Product
+    .find({
+      _id: {
+        $in: [
+          "66db039c34f3f34435263b0b",
+          "66e1959e5a6d26e7383dacb1"
+        ]
+      },
+      deleted: false,
+      status: "active"
+    })
+    .sort({
+      position: "desc"
+    });
+
+  for (const item of productsChoose) {
+    item.priceNew = (1 - item.discountPercentage/100) * item.price;
+    item.priceNew = item.priceNew.toFixed(0);
+  }
+
   res.render("client/pages/home/index", {
-    pageTitle: "Trang chủ"
+    pageTitle: "Trang chủ",
+    productsFeatured: productsFeatured,
+    productsNew: productsNew,
+    productsDiscount: productsDiscount,
+    productsChoose: productsChoose
   });
 }
