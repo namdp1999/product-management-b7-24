dbcab1f5e360d44d6d6934eb6c2591e6f8b7b467
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-10-09 20:39:19 +0700

    2.1. Tạo collection users

---
+++ models/user.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: String,
+    email: String,
+    password: String,
+		token: String,
+    phone: String,
+    avatar: String,
+    status: String,
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
+    deletedAt: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema, "users");
+
+module.exports = User;
