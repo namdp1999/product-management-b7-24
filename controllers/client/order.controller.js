e8299cb074c8f0e94c96282266620076380f408e
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-10-09 20:33:00 +0700

    1.4. Làm trang đặt hàng thành công

--- controllers/client/order.controller.js
+++ controllers/client/order.controller.js
@@ -85,3 +85,39 @@ module.exports.orderPost = async (req, res) => {
 
   res.redirect(`/order/success/${newOrder.id}`);
 }
+
+module.exports.success = async (req, res) => {
+  const orderId = req.params.id;
+
+  const order = await Order.findOne({
+    _id: orderId
+  });
+
+  let total = 0;
+
+  for (const item of order.products) {
+    const infoItem = await Product.findOne({
+      _id: item.productId
+    });
+
+    item.thumbnail = infoItem.thumbnail;
+    item.title = infoItem.title;
+    item.slug = infoItem.slug;
+
+    item.priceNew = item.price;
+    if(item.discountPercentage > 0) {
+      item.priceNew = (1 - item.discountPercentage/100) * item.price;
+      item.priceNew = item.priceNew.toFixed(0);
+    }
+
+    item.total = item.priceNew * item.quantity;
+
+    total += item.total;
+  }
+
+  res.render("client/pages/order/success", {
+    pageTitle: "Đặt hàng thành công",
+    order: order,
+    total: total
+  });
+};
--- routes/client/order.route.js
+++ routes/client/order.route.js
@@ -7,4 +7,6 @@ router.get("/", controller.index);
 
 router.post("/", controller.orderPost);
 
+router.get("/success/:id", controller.success);
+
 module.exports = router;
