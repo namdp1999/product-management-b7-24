7d37dd5512195e64c35044386920548c73a46caf
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-10-14 19:54:46 +0700

    4. Trang Dashboard bên admin

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -1,5 +1,47 @@
-module.exports.index = (req, res) => {
+const Product = require("../../models/product.model");
+
+module.exports.index = async (req, res) => {
+  const statistic = {
+    categoryProduct: {
+      total: 0,
+      active: 0,
+      inactive: 0,
+    },
+    product: {
+      total: 0,
+      active: 0,
+      inactive: 0,
+    },
+    account: {
+      total: 0,
+      active: 0,
+      inactive: 0,
+    },
+    user: {
+      total: 0,
+      active: 0,
+      inactive: 0,
+    },
+  };
+
+  // Sản phẩm
+  statistic.product.total = await Product.countDocuments({
+    deleted: false
+  });
+
+  statistic.product.active = await Product.countDocuments({
+    status: "active",
+    deleted: false
+  });
+
+  statistic.product.inactive = await Product.countDocuments({
+    status: "inactive",
+    deleted: false
+  });
+  // Hết Sản phẩm
+
   res.render("admin/pages/dashboard/index", {
-    pageTitle: "Trang tổng quan"
+    pageTitle: "Trang tổng quan",
+    statistic: statistic
   });
 }
