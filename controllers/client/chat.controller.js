114277c3a5e7cf262e7e20f7526a752710d08be2
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-10-16 20:53:20 +0700

    5.2. Làm tính năng chat cơ bản

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,9 +1,38 @@
+const Chat = require("../../models/chat.model");
+const User = require("../../models/user.model");
+
 module.exports.index = async (req, res) => {
-  _io.on("connection", (socket) => {
-    console.log("Có 1 user kết nối!", socket.id);
+  _io.once("connection", (socket) => {
+    // Người dùng gửi tin nhắn lên server
+    socket.on("CLIENT_SEND_MESSAGE", async (data) => {
+      const dataChat = {
+        userId: res.locals.user.id,
+        // roomChatId: String,
+        content: data.content,
+        // images: Array,
+      };
+
+      // Lưu tin nhắn vào database
+      const chat = new Chat(dataChat);
+      await chat.save();
+    })
   });
+
+  // Lấy tin nhắn mặc định
+  const chats = await Chat.find({
+    deleted: false
+  });
+
+  for (const chat of chats) {
+    const infoUser = await User.findOne({
+      _id: chat.userId
+    });
+
+    chat.fullName = infoUser.fullName;
+  }
 
   res.render("client/pages/chat/index", {
     pageTitle: "Chat",
+    chats: chats
   });
 };
--- public/js/socket.js
+++ public/js/socket.js
@@ -1,1 +1,20 @@
 var socket = io();
+
+// CLIENT_SEND_MESSAGE
+const formChat = document.querySelector(".chat .inner-form");
+if(formChat) {
+  formChat.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const content = formChat.content.value;
+    if(content) {
+      const data = {
+        content: content
+      };
+      socket.emit("CLIENT_SEND_MESSAGE", data);
+
+      formChat.content.value = "";
+    }
+  })
+}
+// End CLIENT_SEND_MESSAGE
