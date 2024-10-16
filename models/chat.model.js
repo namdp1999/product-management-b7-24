d978923865fa29a6be1bcb780dba32d7908bcefc
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-10-16 20:31:45 +0700

    5.1. Tạo collection chats

---
+++ models/chat.model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const chatSchema = new mongoose.Schema(
+  {
+    userId: String,
+    // roomChatId: String,
+    content: String,
+    images: Array,
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
+    deletedAt: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Chat = mongoose.model("Chat", chatSchema, "chats");
+
+module.exports = Chat;
