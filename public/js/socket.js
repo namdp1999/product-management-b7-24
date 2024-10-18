93fc175120dbac04ca0d9b36c54ec743e1939dc7
Đặng Phương Nam <61955753+namdp1999@users.noreply.github.com>
2024-10-18 19:18:42 +0700

    5.3. Hiển thị data realtime - Client hiển thị data ra giao diện

--- public/js/socket.js
+++ public/js/socket.js
@@ -22,5 +22,33 @@ if(formChat) {
 // SERVER_RETURN_MESSAGE
 socket.on("SERVER_RETURN_MESSAGE", (data) => {
   console.log(data);
+  const myId = document.querySelector(".chat").getAttribute("my-id");
+  const body = document.querySelector(".chat .inner-body");
+  const div = document.createElement("div");
+
+  let htmlFullName = "";
+
+  if(myId == data.userId) {
+    div.classList.add("inner-outgoing");
+  } else {
+    div.classList.add("inner-incoming");
+    htmlFullName = `<div class="inner-name">${data.fullName}</div>`;
+  }
+
+  div.innerHTML = `
+    ${htmlFullName}
+    <div class="inner-content">${data.content}</div>
+  `;
+
+  body.appendChild(div);
+
+  body.scrollTop = body.scrollHeight;
 })
 // End SERVER_RETURN_MESSAGE
+
+// Scroll Chat To Bottom
+const bodyChat = document.querySelector(".chat .inner-body");
+if(bodyChat) {
+  bodyChat.scrollTop = bodyChat.scrollHeight;
+}
+// End Scroll Chat To Bottom
